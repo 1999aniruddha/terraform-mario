b66d232a34376f23b9876183afaff11522979629
unknown <katarpanchaniruddha@gmail.com>
2026-01-22 18:53:51 +0530

    Initial Terraform, Kubernetes, and app setup

---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,24 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name = var.cluster_name
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 2
+
+      instance_types = ["t3.micro"]
+    }
+  }
+}
---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,35 @@
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "this" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
